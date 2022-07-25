7a4c9822b5c4724626d4cb20c951771f09a36a6e
DeBLabs <balaji@deblabs.com>
2022-07-25 22:34:46 +0530

    initial commit'

---
+++ src/test/java/com/deblabs/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deblabs.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
